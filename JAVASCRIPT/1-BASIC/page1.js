5b3924778ed2f45cd3af0fe43f7e0150046797a4
Shashank850458 <shashankhebbar85@gmail.com>
2023-10-03 20:33:43 +0530

    3-OCT-23

---
+++ JAVASCRIPT/1-BASIC/page1.js
@@ -0,0 +1,6 @@
+console.log("Hi, Javascript");
+console.log("Banglore");
+console.log("MCA");
+console.log("html \n css \n bootstrap \n javascript");
+
+console.log("html \t css \t bootstrap \t javascript");
---
+++ JAVASCRIPT/1-BASIC/page2.js
@@ -0,0 +1,20 @@
+console.log("What is variable in javascript ?");
+ 
+var a=100;
+console.log(a);
+
+a="banglore";
+console.log(a);
+
+var a=900;
+console.log(a);
+
+console.log("-------using let-------");
+let b= 300;
+console.log(b);
+
+b="500";
+console.log(b);
+
+let b=600;
+console.log(b);
