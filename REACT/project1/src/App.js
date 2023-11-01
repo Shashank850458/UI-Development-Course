4ae7265284df4d9d254dd616d93159f15d386c8e
Shashank850458 <shashankhebbar85@gmail.com>
2023-11-01 20:29:08 +0530

    1-NOV-23

---
+++ REACT/project1/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <xyz>
+      <h1>React is popular in Market</h1>
+      <p>React is javascript code</p>
+      <p>user defined tag added</p>
+    
+    </xyz>
+  );
+}
+
+export default App;
