e6cc13dde40864415bfbd27aa320f0e3f00af4a2
Shashank850458 <shashankhebbar85@gmail.com>
2023-12-17 10:26:34 +0530

    17-12-23

--- REACT/Live Project/project1/src/seller/newproduct.js
+++ REACT/Live Project/project1/src/seller/newproduct.js
@@ -1,12 +1,68 @@
 import { useState,useEffect} from "react";
+import swal from "sweetalert";
+
 
 const Newproduct =() =>{
+    let[pname,pickName] =useState("");
+    let[pprice,pickPrice] =useState("");
+    let[pphoto,pickPhoto] =useState("");
+    let[pdetails,pickDetails] =useState("");
+
+    const save=()=>{
+        let url="http://localhost:1234/product";
+        let productdata={
+            name:pname,
+            price:pprice
+            ,photo:pphoto,
+            details:pdetails,
+            seller:localStorage.getItem("id")
+        };
+        let postdata={
+            headers:{'content-type':'application/json'},
+            method:"POST",
+            body:JSON.stringify(productdata)
+        }
+        fetch(url, postdata)
+        .then(response=>response.json())
+        .then(pdetails=>{
+            swal(pname, "Save Successfully ...", "success");
+            pickName(""); pickPrice("");pickDetails("");pickPhoto("");
+        })
+    }
+
     return (
         <div className="container mt-5">
 
             <div className="row">
-                <div className="col-lg-12 text-center">
+                <div className="col-lg-12 text-center mb-4">
                     <h3 className="text-primary">Add New Product</h3>
+                </div>
+                <div className="col-lg-4 mb-4">
+                    <label>Enter Product Name</label>
+                    <input type="text" className="form-control"
+                         onChange={obj=>pickName(obj.target.value)} value={pname}
+                    />
+                </div>
+                <div className="col-lg-4 mb-4">
+                    <label>Enter Product Price</label>
+                    <input type="text" className="form-control" 
+                    onChange={obj=>pickPrice(obj.target.value)} value={pprice}/>
+                </div>
+                <div className="col-lg-4 mb-4">
+                    <label>Enter Photo URL</label>
+                    <input type="text" className="form-control"
+                         onChange={obj=>pickPhoto(obj.target.value)} value={pphoto}
+                    />
+                </div>
+                
+                <div className="col-lg-12 mb-4">
+                    <label>Enter Product Details</label>
+                  <textarea className="form-control"
+                   onChange={obj=>pickDetails(obj.target.value)} value={pdetails}></textarea>
+                </div>
+                <div className="col-lg-12 text-center">
+                   <br/> <br/>
+                    <button className="btn btn-danger" onClick={save}>Save Product</button>
                 </div>
             </div>
         </div>
--- REACT/Live Project/project1/src/seller/productlist.js
+++ REACT/Live Project/project1/src/seller/productlist.js
@@ -1,12 +1,74 @@
 import { useState,useEffect} from "react";
+import swal from "sweetalert";
 
 const Myproduct =() =>{
+    let[allproduct,updateProduct]=useState([]);
+
+    let sid= localStorage.getItem("id"); //get seller id from localStorage.
+
+    const getproduct =() =>{
+        fetch("http://localhost:1234/product?seller="+sid)
+        .then(response=>response.json())
+        .then(productArray=>{
+            updateProduct(productArray);
+        })
+    }
+    useEffect(()=>{
+        getproduct();
+    },[1]);
+
+    const delproduct = (pid)=>{
+        let url="http://localhost:1234/product/"+pid;
+        let postdata ={method:"delete"}
+        fetch(url,postdata)
+        .then(response=>response.json())
+        .then(emptyres=>{
+            swal("Deleted", "Product Details Deleted", "success");
+            getproduct();
+        })
+    }
     return (
         <div className="container mt-5">
 
             <div className="row">
                 <div className="col-lg-12 text-center">
-                    <h3 className="text-primary">Management Product</h3>
+                    <h3 className="text-primary">
+                    Management Product:{allproduct.length}
+                    </h3>
+                    <table className="mt-4 table table-borderd shadow-lg rounded text-start">
+                        <thead>
+                            <tr>
+                                <th>Pid</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th>Details</th>
+                                <th>Photo</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                allproduct.map((product,index)=>{
+                                    return(
+                                        <tr key={index}>
+                                           <td>{product.id}</td>
+                                           <td>{product.name}</td>
+                                           <td>Rs.{product.price}</td>
+                                           <td>{product.details}</td>
+                                           <td> <img src={product.photo} height="40" width="40"/> </td>
+                                           <td>
+                                              <button className="btn btn-danger btn-sm"
+                                              onClick={delproduct.bind(this, product.id)}>
+                                                <i className="fa fa-trash"></i>
+                                              </button>
+                                            </td>
+                                        </tr>
+                                    )
+                              
+                                })
+                            } 
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
