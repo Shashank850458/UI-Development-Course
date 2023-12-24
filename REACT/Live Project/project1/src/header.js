6fa9c61fc291072312b3f11fd1c391486773f2ff
Shashank850458 <shashankhebbar85@gmail.com>
2023-12-24 08:53:05 +0530

    24-12-2023

--- REACT/Live Project/project1/src/header.js
+++ REACT/Live Project/project1/src/header.js
@@ -1,14 +1,20 @@
 import {Link} from "react-router-dom";
 const CommonHeader =() =>{
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-sm navbar-dark sticky-top bg-dark p-3">
   <div className="container">
-    <a className="navbar-brand"><i className="fa fa-users"></i>User Management</a>
+    <a className="navbar-brand"><i className="fa fa-shpping-bag"></i> Shoppng@Home</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="mynavbar">
       <ul className="navbar-nav ms-auto">
+        <li class="nav-item me-4">
+          <Link className="nav-link active" to="/"><i className="fa fa-home"></i> Shopping</Link>
+        </li>
+        <li class="nav-item me-4">
+          <Link className="nav-link active" to="/cart"><i className="fa fa-shopping-cart"></i> My Cart</Link>
+        </li>
         <li class="nav-item me-4">
           <Link className="nav-link active" to="/register"><i className="fa fa-user-plus"></i> Create Account</Link>
         </li>
--- REACT/Live Project/project1/src/home.js
+++ REACT/Live Project/project1/src/home.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
 
 const Myhome =() =>{
     let[allproduct,updateProduct]=useState([]);
@@ -13,13 +14,53 @@ const Myhome =() =>{
     useEffect(()=>{
         getproduct();
     },[1]);
+
+    let[keyword,updateKeyword] = useState("");  //for search
+    const PER_PAGE = 4;
+    const [currentPage, setCurrentPage] = useState(0);
+    function handlePageClick({ selected: selectedPage }) {
+        setCurrentPage(selectedPage)
+    }
+    const offset = currentPage * PER_PAGE;
+    const pageCount = Math.ceil(allproduct.length / PER_PAGE);
+
+    const addtocart = async(pinfo) =>{
+        let url = "http://localhost:1234/cart";
+        let postdata = {
+            headers:{'Content-Type':'application/json'},
+            method:'POST',
+            body:JSON.stringify(pinfo)
+        }
+        try{
+            await fetch(url,postdata)
+                .then(response => response.json())
+                .then(pinfo => {
+                    alert(pinfo.name+ " Added in your cart", "success");
+            })
+        }
+        catch(error){
+            alert(pinfo.name+" Already in Your Cart", "warning");
+        }
+    }
     return(
         <>
             <section id="banner"></section>
             <div className="container mt-5">
+
+            <div className="row mb-4">
+                <div className="col-lg-4"></div>
+                <div className="col-lg-4">
+                    <input type="text" 
+                    className="form-control"
+                        placeholder="Search..."
+                            onChange={obj=>updateKeyword(obj.target.value)}/>
+                </div>
+                <div className="col-lg-4"></div>
+            </div>
                 <div className="row">
                     {
-                        allproduct.map( (product,index)=>{
+                        allproduct.slice(offset, offset + PER_PAGE).map( (product,index)=>{
+                            if (product.name.toLowerCase().includes(keyword.toLowerCase())|| product.price.includes(keyword))
                             return (
                                 <div className="col-lg-3 mb-4" key={index}>
                                     <div className="p-4 shadow-lg rounded">
@@ -28,7 +69,8 @@ const Myhome =() =>{
                                         <p>{product.details}</p>
                                         <p>Rs. {product.price}</p>
                                         <p className="text-center">
-                                            <button className="btn btn-danger btn-sm">
+                                            <button className="btn btn-danger btn-sm"
+                                            onClick={addtocart.bind(this,product)}>
                                                 <i className="fa fa-shopping-cart"></i> Add to Cart
                                             </button>
                                         </p>
@@ -37,7 +79,28 @@ const Myhome =() =>{
                             )
                         })
                     }
-                </div>
+                </div> {/* Pagination start */}
+                <div className="mb-4 mt-4">
+                        <ReactPaginate
+                            previousLabel={"Previous"}
+                            nextLabel={"Next"}
+                            breakLabel={"..."}
+                            pageCount={pageCount}
+                            marginPagesDisplayed={2}
+                            pageRangeDisplayed={3}
+                            onPageChange={handlePageClick}
+                            containerClassName={"pagination  justify-content-center"}
+                            pageClassName={"page-item "}
+                            pageLinkClassName={"page-link"}
+                            previousClassName={"page-item"}
+                            previousLinkClassName={"page-link"}
+                            nextClassName={"page-item"}
+                            nextLinkClassName={"page-link"}
+                            breakClassName={"page-item"}
+                            breakLinkClassName={"page-link"}
+                            activeClassName={"active primary"}
+                        />
+                    </div>
             </div>
         </>
     )
