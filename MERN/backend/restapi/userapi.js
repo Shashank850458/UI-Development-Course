f01750ccce2e982a9d4a608f790a2e1ffd9c113e
Shashank850458 <shashankhebbar85@gmail.com>
2024-01-03 18:19:43 +0530

    03-01-2024

--- MERN/backend/restapi/userapi.js
+++ MERN/backend/restapi/userapi.js
@@ -4,15 +4,55 @@ module.exports =router;
 
 const User =require("./userschema");
 
-router.get("/",(req,res) => {
-    let msg={"message":"The Get api is working..."};
-    res.status(200).json(msg);
+router.get("/",async(req,res) => {
+    let alluser = await User.find();
+    res.status(200).json(alluser);
 });
 
-router.post("/",(req,res) => {
-    let msg={"message":"The Post api is working..."};
+router.post("/",async(req,res) => {
+    let newuser = User({
+        fullname:req.body.n,
+        mobile:req.body.m,
+        email: req.body.e,
+        address:req.body.a
+    });
+
+    await newuser.save();
+
+    let msg={"message":"New user created successfully!..."};
     res.status(201).json(msg);
 });
+
+
+router.delete("/:id",async(req,res)=>{
+    let userid =req.params.id;
+    let userinfo = await User.findById(userid);
+    if(userinfo==null)
+    {
+        res.status(201).json({"msg":"Invalid user id"});
+    }else{
+        await userinfo.deleteOne();
+        res.status(201).json({"msg":"Record deleted successully..."});
+    }
+});
+
+
+router.put("/",async(req,res)=>{
+     let userid =req.body.id;
+     let userinfo =await User.findById(userid);
+     if(userinfo==null)
+     {
+        res.status(201).json({"msg":"Invalid Request"})
+     }else{
+        userinfo.fullname=req.body.n;
+        userinfo.mobile=req.body.m;
+        userinfo.email= req.body.e;
+        userinfo.address=req.body.a;
+        await userinfo.save();
+        res.status(200).json({"msg":"Record updated successfully"});
+
+     }
+})
 
 
 
